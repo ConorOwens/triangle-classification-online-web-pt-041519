532b5ae48a0dc21db3f1d87b57b543e9e80abc28
Conor Owens <conorj.owens@gmail.com>
2019-05-30 04:29:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end
+  
+  def kind 
+    
+    squared = []
+    squared << @a*@a 
+    squared << @b*@b 
+    squared << @c*@c 
+    squared = squared.sort
+    
+    if @a < 0 || @b < 0 || @c < 0 
+      begin 
+        raise TriangleError
+      end
+    end
+    
+    if @a == 0 || @b == 0 || @c == 0 
+      begin 
+        raise TriangleError
+      end
+    end
+    
+    if (@a + @b <= @c) || (@a + @c <= @b) || (@b + @c <= @a) 
+      begin 
+        raise TriangleError
+      end
+    end
+    
+    if @a == @b && @b == @c
+      return :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    #code?
+  end
+
 end
