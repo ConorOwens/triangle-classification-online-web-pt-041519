79e141860bb0480dec85c664ccfd09d344251ef0
Conor Owens <conorj.owens@gmail.com>
2019-05-30 04:10:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,16 @@ class Triangle
     #code 
   end
   
+  def kind 
+    if @a == @b && @b == @c
+      return :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+  
   class TriangleError < StandardError
     def negatives
       puts "You can't have a negative value for side length!"
