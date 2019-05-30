27e80478bedcbe9b27250312684ffcfea9334def
Conor Owens <conorj.owens@gmail.com>
2019-05-30 04:04:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    #code 
+  end
+  
+  class TriangleError < StandardError
+    if @a < 0 || @b < 0 || @c < 0
+      def message
+  end
 
 end
